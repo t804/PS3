e793f971849601d2f644af082db26104676aed53
t804 <hkomatsu+github@keio.jp>
2019-12-21 10:29:08 +0900

    scratch

--- ps3.py
+++ ps3.py
@@ -1,25 +1,97 @@
 import struct
+import new_motor_speed as motor
 
 device_path = "/dev/input/js0"
+
+debug = 1
 
 # unsigned long, short, unsigned char, unsigned char
 EVENT_FORMAT = "LhBB";
 EVENT_SIZE = struct.calcsize(EVENT_FORMAT)
 
-with open(device_path, "rb") as device:
-  event = device.read(EVENT_SIZE)
-  while event:
-    (ds3_time, ds3_val, ds3_type, ds3_num) = struct.unpack(EVENT_FORMAT, event)
-    if ds3_num == 25:
-      # bring up
-      pass
-    elif ds3_num == 23:
-      pass
-    elif ds3_num == 24:
-      # rotation right
-      pass
-    # if -100<ds3_val and ds3_val<100:
-    else:
-      print( "{0}, {1}, {2}, {3}".format( ds3_time, ds3_val, ds3_type, ds3_num ) )
 
-    event = device.read(EVENT_SIZE)
+def parse_event(event):
+  (time, val, action_type, action_num) = struct.unpack(EVENT_FORMAT, event)
+  action = ""
+  # TODO: 要調査
+  if action_num == 25 and val==0:
+    # ○ボタン押す
+    action = "forward"
+  elif action_num == 25 and val==1:
+    # ○ボタン離す
+    action = "stop"
+  elif action_num == 23:
+    action = "rotate"
+
+  if debug:
+    print("action {0} time: {1}, val: {2}, type: {3}, num: {4}"
+          .format(action, val, action_type, action_num))
+  return (action, time, val, action_type, action_num)
+
+def convert_theta(controller_value):
+  # raw value
+  return controller_value
+
+def step_action(action, action_value, state, state_value):
+  '''
+  Finite-State Automaton
+  '''
+  if action == "forward" and state == "stopped":
+    state = "forward"
+    state_value = convert_theta(0)
+  elif action == "stop" and state == "forward":
+    state = "stopped"  # stoppingを追加する
+  elif action == "rotation" and state == "forward":
+    state = "forward"
+    state_value = convert_theta(state_value)
+  elif debug and action:
+    print("UNKNOWN ACTION: {0}".format(action))
+  return (state, state_value)
+
+def direction2speed(direction_value, default_speed = 5000):
+  MAX_VAL = 32767  # assume right-most value
+  MIN_VAL = -32767  # assume left-most value
+  if direction_value == 0:
+    return (default_speed, default_speed)
+
+  left_bias = 0.
+  right_bias = 0.
+  if direction_value>0:
+    right_bias = 0.5 + 0.5 * (direction_value / MAX_VAL)
+    left_bias = 1. - right_bias
+  elif direction_value<0:
+    left_bias = 0.5 + 0.5 * (direction_value / MIN_VAL)
+    right_bias = 1. - left_bias
+
+  return (default_speed * left_bias,
+          default_speed * right_bias)
+
+
+def execute_action(state, state_value):
+  if state == "forward":
+      (left_speed, right_speed) = direction2speed(state_value)
+      motor.move_both_wheel(left_speed, right_speed)
+
+
+def routine(event, state, state_value):
+  if event:
+    (action, time, val, action_type, action_num) = parse_event(event)
+  (state, state_value) = step_action(event, state, state_value)
+
+  execute_action(state, state_value)
+  return (state, state_value)
+
+def main():
+  motor.init()
+  with open(device_path, "rb") as device:
+    action = "stopped"
+    state = "stopped"
+    state_value = 0
+    while True:
+      event = device.read(EVENT_SIZE)
+      (state, state_value) = routine(event, state, state_value)
+
+if __name__ == __main__:
+  main()
+
+
