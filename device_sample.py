9225c772264998e19a2e5f2777b1b05e6aa28ab4
morux2 <k-morimoto@keio.jp>
2019-12-23 11:11:20 +0900

    update device sample

--- device_sample.py
+++ device_sample.py
@@ -10,7 +10,14 @@ with open(device_path, "rb") as device:
   event = device.read(EVENT_SIZE)
   while event:
     (ds3_time, ds3_val, ds3_type, ds3_num) = struct.unpack(EVENT_FORMAT, event)
-    if -100<ds3_val and ds3_val<100:
-       print( "{0}, {1}, {2}, {3}".format( ds3_time, ds3_val, ds3_type, ds3_num ) )
+    #if -1000<ds3_val and ds3_val<1000:
+    if ds3_type==2 and ds3_num in (4,5,6):
+      # gyrosensor
+      pass
+    elif ds3_type==2 and ds3_num==1:
+      # left stick, vertical move
+      pass
+    else: 
+      print( "time: {0}, value: {1}, type: {2}, number: {3}".format( ds3_time, ds3_val, ds3_type, ds3_num ) )
     
     event = device.read(EVENT_SIZE)
