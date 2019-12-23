43febed914cbd755b81a8b615b140159955682d6
morux2 <k-morimoto@keio.jp>
2019-12-23 12:16:52 +0900

    bugfixes

--- ps3.py
+++ ps3.py
@@ -1,14 +1,20 @@
+#coding: utf-8
 import struct
+import logging
 import lib.new_motor_speed as motor
 
 device_path = "/dev/input/js0"
 
-debug = 1
+debug = 2
 
 # unsigned long, short, unsigned char, unsigned char
-EVENT_FORMAT = "LhBB";
+EVENT_FORMAT = "LhBB"
 EVENT_SIZE = struct.calcsize(EVENT_FORMAT)
 
+'''
+action
+
+'''
 
 def parse_event(event):
   (time, val, action_type, action_num) = struct.unpack(EVENT_FORMAT, event)
@@ -23,9 +29,9 @@ def parse_event(event):
   elif action_type==2 and action_num == 0:
     action = "rotate"
 
-  if debug:
+  if debug==1:
     print("action {0} time: {1}, val: {2}, type: {3}, num: {4}"
-          .format(action, val, action_type, action_num))
+          .format(action, time, val, action_type, action_num))
   return (action, time, val, action_type, action_num)
 
 def convert_theta(controller_value):
@@ -40,19 +46,26 @@ def step_action(action, action_value, state, state_value):
     state = "forward"
     state_value = convert_theta(0)
   elif action == "stop" and state == "forward":
-    state = "stopped"  # stoppingを追加する
-  elif action == "rotation" and state == "forward":
+    state = "stopping"  # stoppingを追加する
+  elif state == "stopping":
+    state = "stopped"
+  elif action == "stop" and state == "stopped":
+    pass  ## unreachable except for initial state
+  elif action == "rotate" and state == "forward":
     state = "forward"
-    state_value = convert_theta(state_value)
+    state_value = convert_theta(action_value)
   elif debug and action:
-    print("UNKNOWN ACTION: {0}".format(action))
+    print("UNKNOWN ACTION: {0}, STATE: {1}".format(action, state))
   return (state, state_value)
 
-def direction2speed(direction_value, default_speed = 5000):
+def direction2speed(direction_value, default_speed = 7000):
   MAX_VAL = 32767  # assume right-most value
   MIN_VAL = -32767  # assume left-most value
   if direction_value == 0:
     return (default_speed, default_speed)
+
+  if direction_value<MIN_VAL or MAX_VAL<direction_value:
+    raise "ERROR: "
 
   left_bias = 0.
   right_bias = 0.
@@ -63,35 +76,42 @@ def direction2speed(direction_value, default_speed = 5000):
     left_bias = 0.5 + 0.5 * (direction_value / MIN_VAL)
     right_bias = 1. - left_bias
 
-  return (default_speed * left_bias,
-          default_speed * right_bias)
+  if debug==2:
+    print("left_bias: {0}, right_bias: {1}".format(left_bias, right_bias))
+  return (int(default_speed * left_bias),
+          int(default_speed * right_bias))
 
 
 def execute_action(state, state_value):
   if state == "forward":
       (left_speed, right_speed) = direction2speed(state_value)
-      motor.move_both_wheel(left_speed, right_speed)
-
+      motor.move_both_wheels(left_speed, right_speed)
+  if state == "stopped":
+      motor.move_both_wheels(0, 0)
 
 def routine(event, state, state_value):
   if event:
     (action, time, val, action_type, action_num) = parse_event(event)
-  (state, state_value) = step_action(event, state, state_value)
+  (state, state_value) = step_action(action, val, state, state_value)
 
   execute_action(state, state_value)
   return (state, state_value)
 
 def main():
   motor.init()
-  with open(device_path, "rb") as device:
-    action = "stopped"
-    state = "stopped"
-    state_value = 0
-    while True:
-      event = device.read(EVENT_SIZE)
-      (state, state_value) = routine(event, state, state_value)
+  try:
+    with open(device_path, "rb") as device:
+      action = ""
+      state = "stopped"
+      state_value = 0
+      while True:
+        event = device.read(EVENT_SIZE)
+        (state, state_value) = routine(event, state, state_value)
+  except Exception as e:
+    motor.move_both_wheels(0,0)
+    logging.exception(e)
 
-if __name__ == __main__:
+if __name__ == "__main__":
   main()
 
 
